6a66ae706ef1067161f527263a49345964fd96ec
SathishFuizen <nimmanasathish31@gmail.com>
2023-03-30 11:24:57 +0530

    opinino pole

---
+++ src/webparts/simplePoll/components/SimplePoll.module.scss.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+require("./SimplePoll.module.css");
+const styles = {
+  simplePoll: 'simplePoll_0247cf27',
+  container: 'container_0247cf27'
+};
+
+export default styles;
+/* tslint:enable */
